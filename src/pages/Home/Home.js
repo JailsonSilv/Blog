86eb37cb07e91e38c2aabc265a01d87dd49aa998
git/ <jailsonsilvaoto@gmail.com>
2022-09-23 11:44:59 -0300

    Todas as página do blog

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,38 @@
+// css
+import styles from './Home.module.css';
+
+// HOOKS 
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+
+// COMPONENTS
+
+const Home = () => {
+  const [query, setQuery] = useState("")
+  const [posts] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className={styles.home}>
+        <h1>Veja os nossos posts mais recentes</h1>
+        <from onSubmit={handleSubmit} className={styles.search_form}>
+          <input type="text" placeholder="Ou busque por tags..." onChange={(e) => setQuery(e.target.value)} />
+          <button className='btn btn-dark'>Pesquisar</button>
+        </from>
+        <div>
+          <h1>Posts..</h1>
+          {posts && posts.length === 0 && (
+            <div className={styles.noposts}>
+              <p>Não foram encontrados posts</p>
+              <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+            </div>
+          )}
+        </div>
+    </div>
+  )
+}
+
+export default Home
